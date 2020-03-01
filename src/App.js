ec159a020984dd3276e70f1d6663be3f885c177b
chutinunbelltan25 <55090817+chutinunbelltan25@users.noreply.github.com>
2020-03-01 20:41:28 +0700

    css sournd code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div class="grid-container">
+                    <div class="grid-item">1แอิ</div>
+                    <div class="grid-item">2</div>
+                    <div class="grid-item">3</div>
+                    <div class="grid-item">4</div>
+                    <div class="grid-item">5</div>
+                    <div class="grid-item">6</div>
+                    <div class="grid-item">7</div>
+                    <div class="grid-item">8</div>
+                    <div class="grid-item">9</div>
+                </div>
+
+                <section class='container'>
+                    <span>*_*</span>
+                    <h1>Hello</h1>
+                    <a href='Bell' class='color'>Bell</a>
+                    <a href='BM' class='colorrr '>BM</a>
+                    <a href='Bank' class='colorhh'>Bank</a>
+                    <div class='box'></div>
+                    <div class='box'></div>
+                    <div class='box'></div>
+                    <div class='box'></div>
+
+                </section>
+                <div class="grid-container2">
+                    <div class="item1">1</div>
+                    <div class="item2">2</div>
+                    <div class="item3">3</div>
+                    <div class="item4">4</div>
+                    <div class="item5">5</div>
+                    <div class="item6">6</div>
+                    <div class="item7"><img class='img1' src='https://scontent.fbkk12-4.fna.fbcdn.net/v/t1.0-9/81113448_119916206174841_4021554780357787648_o.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=WlEHKjSaS88AX8lnFvg&_nc_ht=scontent.fbkk12-4.fna&oh=29e1650ec1368e8b83a21de737d56952&oe=5F038F10' alt='' / ></div>
+                    <div class="item8"><img class='img2' src='https://scontent.fbkk12-4.fna.fbcdn.net/v/t1.0-9/81113448_119916206174841_4021554780357787648_o.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=WlEHKjSaS88AX8lnFvg&_nc_ht=scontent.fbkk12-4.fna&oh=29e1650ec1368e8b83a21de737d56952&oe=5F038F10' alt='' / ></div>
+                    <div class="item9">9</div>
+                    <div class="item10">10</div>
+                    <div class="item11">11</div>
+                    <div class="item12">12</div>
+                    <div class="item13">13</div>
+                    <div class="item14">14</div>
+                    <div class="item15">15</div>
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
